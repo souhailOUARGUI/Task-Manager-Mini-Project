c36d1400c881a75f4756e89358df4cbe1244ff5e
SouhailOUARGUI <ouargui550@gmail.com>
2025-12-23 21:34:54 +0100

    Completed security config and JWT authentication logic

---
+++ backend-task-manager/src/main/java/org/example/backendtaskmanager/security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package org.example.backendtaskmanager.security;
+
+import lombok.RequiredArgsConstructor;
+import org.example.backendtaskmanager.entities.User;
+import org.example.backendtaskmanager.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
