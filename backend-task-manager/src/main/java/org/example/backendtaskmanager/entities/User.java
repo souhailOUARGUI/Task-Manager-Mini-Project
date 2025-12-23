c0645095bf8cd658956fdfa604aff29d17bf13ba
SouhailOUARGUI <ouargui550@gmail.com>
2025-12-23 13:28:56 +0100

    Created the user entity

---
+++ backend-task-manager/src/main/java/org/example/backendtaskmanager/entities/User.java
@@ -0,0 +1,34 @@
+package org.example.backendtaskmanager.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Project> projects = new ArrayList<>();
+}
