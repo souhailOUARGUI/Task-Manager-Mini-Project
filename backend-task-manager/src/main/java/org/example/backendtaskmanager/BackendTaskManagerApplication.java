eede986a203cde56609c7f54f41342c2f17267b3
SouhailOUARGUI <ouargui550@gmail.com>
2025-12-23 12:29:51 +0100

    setup backend project

---
+++ backend-task-manager/src/main/java/org/example/backendtaskmanager/BackendTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendtaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTaskManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTaskManagerApplication.class, args);
+    }
+
+}
---
+++ backend-task-manager/src/test/java/org/example/backendtaskmanager/BackendTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendtaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTaskManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
