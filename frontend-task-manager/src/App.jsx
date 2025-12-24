289673cca9a5b777c0f9d41d0170e39fc1210124
SouhailOUARGUI <ouargui550@gmail.com>
2025-12-24 11:52:04 +0100

    Completed frontend project setup

---
+++ frontend-task-manager/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+   <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Task Manager Frontend is running!!!
+      </h1>
+    </div>
+  )
+}
+
+export default App
