0f6827e8911a621579f9ecc392e6988a3265f4cb
SouhailOUARGUI <ouargui550@gmail.com>
2025-12-24 13:42:17 +0100

    Implemented Login page and edited App.jsx for preview

--- frontend-task-manager/src/App.jsx
+++ frontend-task-manager/src/App.jsx
@@ -1,16 +1,38 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Navbar from "./components/Navbar";
+
+// Pages
+import Login from "./pages/Login";
+import Register from "./pages/Register";     
+import Dashboard from "./pages/Dashboard";
 import './App.css'
 
 function App() {
 
   return (
-   <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-blue-600">
-        Task Manager Frontend is running!!!
-      </h1>
-    </div>
+  <Router>
+      <AuthProvider>
+        <Navbar />
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          <Route element={<ProtectedRoute />}>
+             <Route path="/dashboard" element={<Dashboard />} />
+             <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          </Route>
+
+          {/* Redirect unknown Urls to login page */}
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      </AuthProvider>
+    </Router>
   )
 }
 
---
+++ frontend-task-manager/src/pages/Dashboard.jsx
@@ -0,0 +1,3 @@
+const Dashboard = () => <div>Dashboard Page (Coming Soon)</div>;
+
+export default Dashboard;
